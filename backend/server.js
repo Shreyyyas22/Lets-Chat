8d1fbbc9b78c5716fd25c7ebda69fe4de318bf35
Shreyash Wagh <shreyashwagh22@gmail.com>
2024-09-30 12:07:11 +0530

    Project completed

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,8 @@ import userRoutes from "./routes/user.js"
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import cookieParser from "cookie-parser";
+import { app, server} from "./socket/socket.js"
 
-const app = express();
 
 const PORT = process.env.PORT || 3000;
 dotenv.config()
@@ -24,7 +24,7 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     connectToMongoDB()
     console.log(`Server is running on port ${PORT}`)
 })
