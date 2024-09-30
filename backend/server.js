ec49e58722d1a8244c3e6af32616420a6d2a9126
Shreyash Wagh <shreyashwagh22@gmail.com>
2024-09-30 12:22:35 +0530

    Project Completed

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from "express"
 import dotenv from "dotenv"
 
@@ -11,18 +12,24 @@ import { app, server} from "./socket/socket.js"
 
 
 const PORT = process.env.PORT || 3000;
+
+const __dirname = path.resolve()
+
 dotenv.config()
 
 app.use(express.json())
 app.use(cookieParser())
 
-// app.get("/", (req, res) => {
-//     res.send("Hello World!");
-// })
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 server.listen(PORT, () => {
     connectToMongoDB()
